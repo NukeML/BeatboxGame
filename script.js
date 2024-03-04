53d6d4a6fa6b767991af2766803967727b16942b
NukeML <162136946+NukeML@users.noreply.github.com>
2024-03-04 18:10:04 +0000

    Update script.js

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ function playRandomReferenceClip() {
       referenceClip.src = data.randomClipUrl;
     });
   alert(JSON.stringify(data));
-  document.getElementById('rcText').textContent = JSON.stringify(data);
+  document.getElementById('rcText').textContent = "fanchoeng";
 }
 
 // Function to record user's mimicry attempt

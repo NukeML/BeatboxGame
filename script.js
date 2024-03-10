d1b4de0d6cd605f9a0a4829daa929503a514b75a
NukeML <162136946+NukeML@users.noreply.github.com>
2024-03-10 18:48:00 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,61 @@
+const visualizer = document.querySelector(".visualizer");
+
+var audioCtx;
+const canvasCtx = visualizer.getContext("2d");
+
+
+
+function visualize(stream) {
+  if (!audioCtx) {
+    audioCtx = new AudioContext();
+  }
+
+  const source = audioCtx.createMediaStreamSource(stream);
+  const analyser = audioCtx.createAnalyser();
+  analyser.fftSize = 4096;
+  const bufferLength = analyser.frequencyBinCount;
+  const dataArray = new Uint8Array(bufferLength);
+
+  source.connect(analyser);
+
+  draw(visualizer, analyser, bufferLength);
+}
+
+function draw(analyser, bufferLength) {
+  const vWidth = visualizer.width;
+  const vHeight = visualizer.height;
+
+  requestAnimationFrame(draw);
+  analyser.getByteTimeDomainData(dataArray);
+
+  canvasCtx.fillStyle = "rgb(200, 200, 200)";
+  canvasCtx.fillRect(0, 0, vWidth, vHeight);
+  canvasCtx.lineWidth = 2;
+  canvasCtx.strokeStyle = "rgb(0, 0, 0)";
+  canvasCtx.beginPath();
+
+  var sliceWidth = (vWidth * 1.0) / bufferLength;
+  var x = 0;
+
+  for (let i = 0; i < bufferLength; i++) {
+    let v = dataArray[i] / 128.0;
+    let y = (v * vHeight) / 2;
+
+    if (i === 0) {
+      canvasCtx.moveTo(x, y);
+    } else {
+      canvasCtx.lineTo(x, y);
+    }
+
+    x += sliceWidth;
+    
+  }
+
+  canvasCtx.lineTo(vWidth, vHeight / 2);
+  canvasCtx.stroke();
+  
+}
+
 // Function to fetch and play a random reference clip
 function playRandomReferenceClip() {
   // Make an HTTP request to Google Apps Script to get the URL of a random reference clip
